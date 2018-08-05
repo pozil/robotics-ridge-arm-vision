0f4817162199fd31fda00281f56f026f2434f0d7
pozil <>
2018-08-05 21:26:32 +0200

    Added test script

---
+++ test-picture.js
@@ -0,0 +1,65 @@
+require('dotenv').config();
+const Winston = require('winston'),
+  httpClient = require("request"),
+  ARM = require('./src/arm'),
+  SalesforcePlatform = require('./src/salesforce-platform'),
+  os = require('os');
+
+const HOSTNAME = process.env.hostname || os.hostname();
+
+// Configure logs
+Winston.loggers.add('App', {
+  console: { level: 'info', colorize: true, label: 'App' }
+});
+const LOG = Winston.loggers.get('App');
+
+Winston.default.transports.console.level='debug';
+Winston.loggers.get('App').transports.console.level='debug';
+Winston.loggers.get('ARM').transports.console.level='debug';
+Winston.loggers.get('SFDC').transports.console.level='debug';
+
+
+process.on('warning', e => console.warn(e.stack));
+process.on('unhandledRejection', (reason, p) => {
+  LOG.error('Unhandled Rejection at: Promise', p, 'reason:', reason);
+});
+
+const sfdc = new SalesforcePlatform(HOSTNAME);
+const arm = new ARM();
+
+function sleep(duration) {
+  return new Promise((resolve, reject) => {
+    LOG.debug('Sleep', duration);
+    setTimeout(() => {
+      resolve();
+    }, duration);
+  });
+}
+
+
+startApp = () => {
+  sfdc.init(onArmPickupRequested, onArmPickupConfirmed)
+  .then(() => {
+    return arm.init(false);
+  })
+  .then(() => {
+    return arm.positionToCapturePicture();
+  })
+  .then(() => {
+    return sleep(1000);
+  })
+  .then(() => {
+    return arm.capturePicture();
+  })
+  .then((picture) => {
+    return sfdc.uploadPicture(picture)
+  })
+  .catch((error) => {
+    LOG.error(error);
+  });
+}
+
+onArmPickupRequested = () => {}
+onArmPickupConfirmed = () => {}
+
+startApp();
