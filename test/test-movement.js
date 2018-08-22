0aaa69dada3036ad423cb3468aaa6c59cd173208
Reid Carlberg <reid.carlberg@gmail.com>
2018-08-22 15:20:13 -0600

    test file and testconfig covers basics

--- test/test-movement.js
+++ test/test-movement.js
@@ -16,3 +16,29 @@ it ('should be fine executing only the setup dof0 = 101', function() {
 	mover.handleMove(mover.positions.onlySetup);
 	assert.equal(mover.currentPosition["dof0", 101]);
 });
+
+it ('should be fine executing only the move dof0 = 102', function() {
+	mover.handleMove(mover.positions.onlyMove);
+	assert.equal(mover.currentPosition["dof0", 102]);
+});
+
+it ('should be fine doing combo moves of setup and move dof0 = 104', function() {
+	mover.handleMove(mover.positions.setupAndMove);
+	assert.equal(mover.currentPosition["dof0",104]);
+});
+
+it ('should go through the whole sequence ending at exit dof0 = 108', function() {
+	mover.handleMove(mover.positions.example);
+	assert.equal(mover.currentPosition["dof0", 108]);
+});
+
+it ('should work with a single DOF dof0 = 109', function() {
+	mover.handleMove(mover.positions.singleDof);
+	assert.equal(mover.currentPosition["dof0", 109]);
+});
+
+it ('should work with a couple of DOFs which are single in their step', function() {
+	mover.handleMove(mover.positions.doubleDof);
+	assert.equal(mover.currentPosition["dof0", 110]);
+	assert.equal(mover.currentPosition["dof1", 111]);
+});
