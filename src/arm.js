68dd56256fe55d230f089bcc3c14bd89b3183134
Reid Carlberg <reid.carlberg@gmail.com>
2018-09-04 15:41:19 -0700

    configure and test four hotspots

--- src/arm.js
+++ src/arm.js
@@ -9,15 +9,14 @@ Winston.loggers.add('ARM', {
 const LOG = Winston.loggers.get('ARM');
 
 module.exports = class ARM {
-
   constructor(hostname) {
     this.hostname = hostname;
     this.mover = new Movement(this.hostname);
     this.camera = new Raspistill({
       noFileSave: true,
       verticalFlip: false,
-      width: 800,
-      height: 600,
+	width: 800,
+	height: 600,
       time: 0
     });
     this.currentTarget = '';
@@ -68,30 +67,32 @@ module.exports = class ARM {
     probabilities.forEach(probability => {
       const box = probability.boundingBox;
       probability.center = {
-        x : box.maxX - box.minX,
-        y : box.maxY - box.minY,
+        x : ((box.maxX - box.minX) / 2) + box.minX,
+        y : ((box.maxY - box.minY) / 2) + box.minY,
       };
     });
+
+	console.log(probabilities);
 
     probabilities.forEach(probability => {
       if (probability.label.toLowerCase() == mytarget) {
 
-        if (probability.center.x >= 90 && probability.center.x <= 115 && 
-          probability.center.y >= 95 && probability.center.y < 115) {
-          ret = this.mover.goPickupFour();
-		console.log('position 4');
-        } else if (probability.center.x >= 120 && probability.center.x <= 130 && 
-          probability.center.y >= 110 && probability.center.y < 120) {
-            ret = this.mover.goPickupThree();
-		console.log('position 3');
-        } else if (probability.center.x >= 110 && probability.center.x <= 120 && 
-          probability.center.y >= 105 && probability.center.y < 115) {
-            ret = this.mover.goPickupTwo();
-		console.log('position 2');
-        } else if (probability.center.x >= 100 && probability.center.x <= 125 && 
-          probability.center.y >= 110 && probability.center.y < 130) {
+        if (probability.center.x >= 350 && probability.center.x <= 400 && 
+          probability.center.y >= 465 && probability.center.y <= 515) {
           ret = this.mover.goPickupOne();
 		console.log('position 1');
+        } else if (probability.center.x >= 485 && probability.center.x <= 535 && 
+          probability.center.y >= 255 && probability.center.y <= 305) {
+            ret = this.mover.goPickupTwo();
+		console.log('position 2');
+        } else if (probability.center.x >= 165 && probability.center.x <= 215 && 
+          probability.center.y >= 225 && probability.center.y < 275) {
+            ret = this.mover.goPickupThree();
+		console.log('position 3');
+        } else if (probability.center.x >= 375 && probability.center.x <= 425 && 
+          probability.center.y >= 105 && probability.center.y < 155) {
+          ret = this.mover.goPickupFour();
+		console.log('position 4');
         }
       }
     });   
